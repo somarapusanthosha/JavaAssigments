d4aaf097f620ddd470f42dd70d328cda98c796e4
somarapusanthosha <123785487+somarapusanthosha@users.noreply.github.com>
2023-03-09 19:59:33 -0600

    Push

--- src/com/assignment/Colors.java
+++ src/com/assignment/Colors.java
@@ -3,5 +3,7 @@ package com.assignment;
 public class Colors {
     public static void main(String[] args) {
 
+        System.out.println("Hello Colors");
+        System.out.println("Hello Colors");
     }
 }
